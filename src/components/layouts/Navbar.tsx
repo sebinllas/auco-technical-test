ba760957d5295791daf196d7779837c85e9cacb8
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-09-10 21:26:00 -0500

    Change minor styling details on Navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -7,12 +7,14 @@ import { Theme } from '@/types/Theme';
 export const Navbar = () => {
 	const [theme, toggleTheme] = useTheme();
 	return (
-		<nav className="flex items-end gap-4 p-2 pb-4 border-slate-300 dark:border-slate-700 shadow-black/20 shadow-md border-b sticky top-0 left-0 bg-gray-200 dark:bg-gray-700/40 backdrop-blur-md">
-			<div className="px-4 flex flex-col justify-center">
-				<span className="text-xs font-thin text-gray-600 dark:text-gray-400">Technical test</span>
+		<nav className="flex flex-wrap items-end gap-2 sm:gap-4 p-2 pb-4 border-slate-300 dark:border-slate-700 
+		shadow-black/20 shadow-md border-b sticky top-0 left-0 bg-gray-200 dark:bg-gray-700/40 font-bold
+		backdrop-blur-md text-gray-700 dark:text-white sm:px-8 px-2">
+			<div className="flex flex-col justify-center shrink-0">
+				<span className="text-xs font-extralight dark:text-gray-200">Technical test</span>
 				<Image src="/auco.png" width={90} height={18} alt="Auco logo" />
 			</div>
-			<ul className="flex gap-4 grow">
+			<ul className="flex justify-end gap-2 sm:gap-4 text-sm sm:text-base grow flex-wrap">
 				<li>
 					<Link href={'/'}>Home</Link>
 				</li>
