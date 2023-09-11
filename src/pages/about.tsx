6df44234a7e9f5992dbb5e8a88c75d97da71b26b
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-09-11 16:53:21 -0500

    Add content in about me page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
 
 export const About = () => {
-  return (
-    <div>About Me</div>
-  )
-}
+	return (
+		<main className="[&>p]:my-4">
+			<h1 className="text-3xl font-bold mb-4 flex gap-4">Sobre Mí 👨‍💻</h1>
+			<p>
+				A corto plazo, mi visión se centra en completar mi carrera en ingeniería
+				de sistemas 🎓 y lanzarme a trabajar en proyectos que me desafíen y
+				enriquezcan tanto en términos técnicos como personales. Mi motivación
+				principal es adquirir experiencia en el desarrollo de productos que no
+				solo sean técnicamente sólidos 🛠️, sino que también agreguen un valor
+				real a las personas y resuelvan problemas concretos 🧩.
+			</p>
+
+			<p>
+				Con las habilidades que he desarrollado en la universidad 🏫 y mi
+				experiencia como auxiliar de programación 💻, donde contribuí al
+				desarrollo del sitio web del departamento de ingeniería de sistemas{' '}
+				<a href="https://sistemas.udea.edu.co" className="underline ">
+					sistemas.udea.edu.co
+				</a>{' '}
+				🌐, estoy seguro de que puedo aportar eficazmente a los objetivos de{' '}
+				<strong>Auco</strong>. Mi pasión por aprender 📚 y trabajar en
+				soluciones prácticas que beneficien a las personas me impulsa a
+				contribuir de manera significativa a proyectos desafiantes 💪.
+			</p>
+
+			<p>
+				Si bien aún estoy aprendiendo sobre la cultura de <strong>Auco</strong>,
+				me atrae su enfoque hacia la reducción en el uso de recursos 🌱,
+				disminución de de costos operativos y mejora de los procesos de gestión
+				documental a través de soluciones electrónicas 📄➡️📱.
+			</p>
+
+			<p>
+				A medida que avanzo en mi carrera, me encantaría crecer junto con{' '}
+				<strong>Auco</strong>, una startup que veo como un lugar lleno de
+				oportunidades para mi desarrollo profesional y personal 🚀. Estoy
+				emocionado por la posibilidad de contribuir a su éxito y trabajar en
+				proyectos que impacten positivamente en la sociedad 🌍.
+			</p>
+
+			<p>
+				Mis objetivos profesionales van encaminados a seguir aprendiendo 📚,
+				aplicando mis conocimientos 💡 y adaptándome a los nuevos desafíos
+				tecnológicos 🧠, y creo que <strong>Auco</strong> podría ser el lugar
+				perfecto para ello.
+			</p>
+
+			<p>
+				¡Estoy emocionado por la posibilidad de ser parte de su equipo y espero
+				poder colaborar con ustedes 🤝 en la construcción de un futuro
+				tecnológico brillante! 🌟 
+			</p>
+		</main>
+	);
+};
 
 export default About;
